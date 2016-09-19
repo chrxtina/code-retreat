b4502084b1777a0670c7aa51c7a899a1d2abd817
chrxtina <christina.ad.nguyen@gmail.com>
2016-09-19 15:29:15 -0400

    Define populate_board

--- lib/conway.rb
+++ lib/conway.rb
@@ -1,13 +1,23 @@
 class World
+  attr_read :length :width :cells
   def initialize(length, width)
     @length = length
     @width = width
     @cells = []
   end
+
+  def populate_board
+    width.times do |row|
+      row = []
+      length.times do |column|
+        row << Cell.new
+      end
+    end
+  end
 end
 
 class Cell
   def initialize
-    state
+    @alive = false
   end
 end
